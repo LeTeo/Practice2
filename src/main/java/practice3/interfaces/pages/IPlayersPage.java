59fa74de650b50794f230b7afad7ca468bb92f63
leTeo <kondratovolexiy@gmail.com>
2016-12-06 23:59:21 +0300

    Third commit. ADD: More comments, methods. UPD: CRUDTests. DEL: Dependencies in tests, many warnings.

--- src/main/java/practice3/interfaces/pages/IPlayersPage.java
+++ src/main/java/practice3/interfaces/pages/IPlayersPage.java
@@ -1,11 +1,7 @@
 package practice3.interfaces.pages;
 
-import practice3.interfaces.IPokerPlayer;
-import practice3.pages.PlayersPage;
-
 /**
- * Created by ly0w on 04.12.2016.
+ * Created by ly0w on 06.12.2016.
  */
 public interface IPlayersPage {
-    IPokerPlayer insertPlayerClone(PlayersPage playersPage, IPokerPlayer pokerPlayer, IEditPlayerPage editPlayerPage);
 }
